0209314866e67adbcfcc8cf396b343d10f25232b
dnttr <147146363+dnttr@users.noreply.github.com>
2025-05-25 11:56:53 +0200

    initial commit: add main application structure and POM file

---
+++ src/main/java/Loader.java
@@ -0,0 +1,10 @@
+/**
+ * @author dnttr
+ */
+
+public class Loader {
+
+    public static void main(String[] args) {
+        //load native library, soonTM
+    }
+}
---
+++ src/main/java/org/dnttr/zephyr/management/bridge/Native.java
@@ -0,0 +1,12 @@
+package org.dnttr.zephyr.management.bridge;
+
+/**
+ * @author dnttr
+ */
+
+public final class Native {
+
+    public Native() {
+        //load the native library here, add bindings
+    }
+}
