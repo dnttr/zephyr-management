e9642d7218680801e84abbe9b64f7925ef9d071b
dnttr <147146363+dnttr@users.noreply.github.com>
2025-06-07 19:10:33 +0200

    Refactor Bindings class: rename session management methods and remove no longer necessary encryption methods

--- src/main/java/org/dnttr/zephyr/management/Bindings.java
+++ src/main/java/org/dnttr/zephyr/management/Bindings.java
@@ -20,10 +20,10 @@ public final class Bindings {
     /**
      * Creates a new session.
      *
-     * @param b1_ptr buffer to store the session identifier
+     * @param u session identifier
      * @return 0 on success, non-zero on failure
      */
-    public native int ffi_zm_open_session(long[] b1_ptr);
+    public native int ffi_open_session(long u);
 
     /**
      * Closes an existing session.
@@ -31,65 +31,7 @@ public final class Bindings {
      * @param u unique identifier for the session
      * @return 0 on success, non-zero on failure
      */
-    public native int ffi_zm_close_session(long u);
+    public native int ffi_close_session(long u);
 
-    /**
-     * Encrypts a message in symmetric mode.
-     *
-     * @param u unique identifier for the session
-     * @param b1 buffer containing message to process
-     * @param b2 buffer containing aead
-     * @param b3_ptr buffer to store the encrypted message
-     * @return 0 on success, non-zero on failure
-     */
-    public native int ffi_ze_encrypt_symmetric(long u, byte[] b1, byte[] b2, byte[] b3_ptr);
-
-    /**
-     * Decrypts a message in symmetric mode.
-     *
-     * @param u unique identifier for the session
-     * @param b1 buffer containing message to process
-     * @param b2 buffer containing aead
-     * @param b3_ptr buffer to store the decrypted message
-     * @return 0 on success, non-zero on failure
-     */
-    public native int ffi_ze_decrypt_symmetric(long u, byte[] b1, byte[] b2, byte[] b3_ptr);
-
-    /**
-     * Encrypts a message in asymmetric mode.
-     *
-     * @param u unique identifier for the session
-     * @param b1 buffer containing message to process
-     * @param b2_ptr buffer to store the encrypted message
-     * @return 0 on success, non-zero on failure
-     */
-    public native int ffi_ze_encrypt_asymmetric(long u, byte[] b1, byte[] b2_ptr);
-
-    /**
-     * Decrypts a message in asymmetric mode.
-     *
-     * @param u unique identifier for the session
-     * @param b1 buffer containing message to process
-     * @param b2_ptr buffer to store the decrypted message
-     * @return 0 on success, non-zero on failure
-     */
-    public native int ffi_ze_decrypt_asymmetric(long u, byte[] b1, byte[] b2_ptr);
-
-
-    /**
-     * Generates a nonce for the session.
-     * It isn't necessary to call it every time you want to encrypt a message, however from security standpoint it is recommended to call it before each encryption operation.
-     * The nonce itself is stored on the native side.
-     * <br><br>
-     * Modes of operation:
-     * <br>
-     * 0 - generate nonce for asymmetric encryption
-     * <br>
-     * 1 - generate nonce for symmetric encryption
-     * <br>
-     * @param u unique identifier for the session
-     * @param m mode of operation
-     * @return 0 on success, non-zero on failure
-     */
-    public native int ffi_ze_nonce(long u, int m);
+    public native int ffi_push(long uuid, byte[] data);
 }
