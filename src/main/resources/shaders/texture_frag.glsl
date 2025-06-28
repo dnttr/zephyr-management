3899e33639dc20bc8841d36d548c0ec48056656d
dnttr <147146363+dnttr@users.noreply.github.com>
2025-06-29 00:54:38 +0200

    Add tex_alpha uniform and alpha thresholding to texture fragment shader for improved transparency control

--- src/main/resources/shaders/texture_frag.glsl
+++ src/main/resources/shaders/texture_frag.glsl
@@ -5,7 +5,15 @@ out vec4 FragColor;
 in vec2 coords;
 
 uniform sampler2D tex;
+uniform float tex_alpha;
 
 void main() {
-    FragColor = texture(tex, coords);
+    vec4 finalColor = texture(tex, coords);
+
+    if (finalColor.a < 0.01) {
+        discard;
+    }
+
+    finalColor.a = tex_alpha * smoothstep(0.01, 1.0, finalColor.a);
+    FragColor = finalColor;
 }
