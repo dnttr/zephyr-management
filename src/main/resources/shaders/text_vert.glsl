81074296fd356d134ddbff7740285ed808bf746b
dnttr <147146363+dnttr@users.noreply.github.com>
2025-06-28 22:49:56 +0200

    Add animated scaling to text shader with configurable speed and scale parameters

--- src/main/resources/shaders/text_vert.glsl
+++ src/main/resources/shaders/text_vert.glsl
@@ -7,11 +7,17 @@ layout(location = 2) in float characterIndex;
 flat out int charIndex;
 out vec2 TexCoords;
 
+uniform float time;
+uniform float maxScale;
+uniform float startingScale;
+uniform float speedScaling;
 uniform mat4 projection;
 
 void main()
 {
-    gl_Position = projection * vec4(position, 0.0, 1.0);
+    float charOffset = float(charIndex) * 0.2f;
+    float scale = min(maxScale, startingScale + time * speedScaling);
+    gl_Position = projection * vec4(position * scale, 0.0, 1.0);
     charIndex = int(characterIndex);
     TexCoords = texCoord;
 }
